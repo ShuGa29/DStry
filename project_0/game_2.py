e5faf19b979f5064dfcf4d093fa83c5fa5b9e196
ShuGa29 <andraiser@yandex.ru>
2022-03-13 16:20:47 +0300

    change

--- project_0/game_2.py
+++ project_0/game_2.py
@@ -1,44 +1,41 @@
+"""Игра - "Угадай число", где компьютер сам загадывает число и возвращает среднее значение попыток"""
+ 
 import numpy as np
 
-def random_predict(number:int=1) -> int:
-    """Рандомно угадываем число
-    
-    Args:
-        number (int, optional): Загаданное число. Defaults to 1.
+def random_predict(number:int=1) -> int: # Функция подсчета количества попыток
+   
+    count = 0 # Счетчик попыток
+    left_edge = 1 # Граница минимального значения
+    right_edge = 101 # Граница максимального значения
         
-    Returns:
-        int: Число попыток"""
-        
-    count = 0
-    left_edge = 1
-    right_edge = 101
-        
-    
-        
-    while True:
+    while True: # Проходим циклом по диапазону значений
         count += 1
-        predict_number = (right_edge + left_edge) // 2
-        if number < predict_number:
+        predict_number = (right_edge + left_edge) // 2 
+        
+        if number < predict_number: # Задаем условие для сужения дапазона поиска числа и уменьшения числа попыток
             right_edge = predict_number
+            
         elif number > predict_number:
             left_edge = predict_number
+            
         else:
             break
+        
     return(count)
 
 def score_game(random_randit) -> int:
     
-    """За какое количество попыток в среднем за 1000 подходов угадывает наш алгоритм
+    """Функция подсчета количества попыток, которое в среднем угадывает наш алгоритм за 1000 подходов 
     
     Args:
-        random_ predict ([type]): функция угадывания
+        random_ predict ([type]): функция угадывания числа
         
     Returns:
         int: среднее количество попыток"""
     
     count_ls = []
-    np.random.seed(1) # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+    np.random.seed(1) # Фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # Загадали список чисел
     
     for number in random_array:
         count_ls.append(random_predict(number))
